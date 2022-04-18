668826019d74faf124874986280d244ceadd0a9f
Max Oblein <max.oblein@gmail.com>
2022-04-18 17:18:30 +0100

    removed logger

--- faceID.py
+++ faceID.py
@@ -34,7 +34,7 @@ class CamApp(App):
         layout.add_widget(self.verification_label)
 
         #load keras model
-        self.model = tf.keras.models.load_model('model_versions/siamesemodelv2.h5', custom_objects={'L1Dist':L1Dist})
+        self.model = tf.keras.models.load_model('model_versions/siamesemodel.h5', custom_objects={'L1Dist':L1Dist})
 
         #setup video capture
         self.capture = cv2.VideoCapture(0)
@@ -44,7 +44,7 @@ class CamApp(App):
         return layout
 
     def update(self, *args):
-        
+
         #read frame from open cv
         ret, frame = self.capture.read()
         frame = frame[120:120+250,200:200+250,:]
@@ -56,20 +56,20 @@ class CamApp(App):
         self.web_cam.texture=img_texture
 
     def preprocess(self,file_path):
-    
+
         # Read in image from file path
         byte_img = tf.io.read_file(file_path)
-        # Load in the image 
+        # Load in the image
         img = tf.io.decode_jpeg(byte_img)
-        
+
         # Preprocessing steps - resizing the image to be 100x100x3
         img = tf.image.resize(img, (100,100))
-        # Scale image to be between 0 and 1 
+        # Scale image to be between 0 and 1
         img = img / 255.0
 
         # Return image
         return img
-    
+
     #used to verify our image
     def verify(self, *args):
         #specify thresholds
@@ -81,7 +81,7 @@ class CamApp(App):
         ret, frame = self.capture.read()
         frame = frame[120:120+250,200:200+250,:]
         cv2.imwrite(SAVE_PATH,frame)
-    
+
 
         #detection threshold: Metric above which a prediction is considered positive
         #verification threshold: number of positive matches with validation images
@@ -106,9 +106,6 @@ class CamApp(App):
         #update verification text
         self.verification_label.text = 'Verified' if verified == True else 'Unverified'
 
-        Logger.info(results)
-        Logger.info(detection)
-        Logger.info(verification)
 
         return results, verified
 
